7d313626881d1f1b2fc0b4f234123c9747213ef8
Esteban Spina <esteban.spina@globant.com>
2016-11-25 10:34:01 -0300

    Initial Commit

---
+++ vagrant/init.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+GIT_EMAIL="user@domain.com"
+GIT_USER="User Name"
+MYSQL_PASSWORD=""
+
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+echo "Provisioning virtual machine..."
+
+echo "- Installing Git."
+sudo apt-get install -y git
+
+echo "- Configuring Git."
+git config --global user.name $GIT_USER
+git config --global user.email $GIT_EMAIL
+
+echo "- Installyng Ruby."
+sudo apt-get install -y ruby
+
+echo "- Installing Apache2."
+sudo apt-get install -y apache2
+
+echo "- Configuring Apache2."
+grep -q "ServerName localhost" /etc/apache2/apache2.conf || sudo echo "ServerName localhost" >> /etc/apache2/apache2.conf
+sudo a2dissite 000-default.conf
+sudo cp /var/www/vagrant/init.vhost /etc/apache2/sites-available/000-default.conf
+sudo a2ensite 000-default.conf
+sudo ufw allow in "Apache Full"
+
+echo "- Testing Apache2."
+sudo apache2ctl configtest
+
+echo "- Restarting Apache2."
+sudo service apache2 restart
+
+echo "- Setting required MySQL configurations."
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_PASSWORD'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD'
+
+echo "- Installing MySQL."
+sudo apt-get install -y mysql-server
+
+echo "- Installing PHP."
+sudo apt-get install -y php7.0
+sudo apt-get install -y libapache2-mod-php7.0
+sudo apt-get install -y php7.0-mysql
+sudo apt-get install -y php7.0-xml
+
+echo "- Configuring PHP."
+sudo a2enmod rewrite
+
+echo "- Restarting Apache2."
+sudo service apache2 restart
+
+echo "- Installing Composer."
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+echo "- Cleaning unnecesary packages."
+sudo apt-get autoremove -y --purge
